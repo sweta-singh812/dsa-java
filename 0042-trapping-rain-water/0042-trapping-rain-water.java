837e83d90d9e434a4ba47939b9727b30fd128dc1
SWETASINGH <sweta_scsbs2024@msit.edu.in>
2026-07-20 22:16:38 +0530

    Time: 0 ms (100%), Space: 47.6 MB (82.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,39 +1,37 @@
 class Solution {
-
     public int trap(int[] height) {
 
-        int left = 0;
-        int right = height.length - 1;
+        int left=0;
+        int right= height.length -1;
 
-        int leftMax = 0;
-        int rightMax = 0;
+        int leftmax=0;
+        int rightmax=0;
 
-        int water = 0;
+        int water= 0;
 
-        while (left < right) {
+        while(left<right) {
+            if(height[left] < height[right]) {
 
-            if (height[left] < height[right]) {
+                if (height[left]>= leftmax){
 
-                if (height[left] >= leftMax) {
-
-                    leftMax = height[left];
-
-                } else {
-
-                    water += leftMax - height[left];
+                    leftmax= height[left];
                 }
 
+                else{
+
+                    water += leftmax - height[left];
+                }
                 left++;
+            }
+            else{
+                if(height[right]>= rightmax){
 
-            } else {
+                    rightmax= height[right];
+                }
 
-                if (height[right] >= rightMax) {
+                else{
 
-                    rightMax = height[right];
-
-                } else {
-
-                    water += rightMax - height[right];
+                    water += rightmax - height[right];
                 }
 
                 right--;
@@ -41,7 +39,6 @@ class Solution {
         }
 
         return water;
+        
     }
 }
-        
-    
