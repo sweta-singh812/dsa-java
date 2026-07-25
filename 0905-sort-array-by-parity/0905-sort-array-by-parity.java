1d624a091c60d3b8afa2437ead4885270f58bf1a
SWETASINGH <sweta_scsbs2024@msit.edu.in>
2026-07-25 21:23:20 +0530

    Time: 1 ms (49.82%), Space: 46.6 MB (94%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -2,17 +2,26 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
 
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length -1;
 
-        while (left < right) {
+        while(left<right){
 
-            if (nums[left] % 2 == 0) {
+            if(nums[left] %2 == 0){
+
                 left++;
-            } 
-            else if (nums[right] % 2 == 1) {
+
+            }
+
+            else if(nums[right] % 2 == 1) {
+
                 right--;
-            } 
-            else {
+
+
+
+
+            }
+
+            else{
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
@@ -20,8 +29,11 @@ class Solution {
                 left++;
                 right--;
             }
-        }
+
+            
+        } 
 
         return nums;
+        
     }
 }
