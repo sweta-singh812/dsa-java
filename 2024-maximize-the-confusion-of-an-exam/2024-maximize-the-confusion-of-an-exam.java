8a9bc3afd4ff6c126bcda2dacd18a2edd84f92b9
SWETASINGH <sweta_scsbs2024@msit.edu.in>
2026-08-01 00:48:01 +0530

    Time: 15 ms (50%), Space: 46.6 MB (31.32%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+
+        int changeF = findMax(answerKey, k, 'F');
+        int changeT = findMax(answerKey, k, 'T');
+
+        return Math.max(changeF, changeT);
+    }
+
+    private int findMax(String answerKey, int k, char changeChar) {
+
+        int left = 0;
+        int changeCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < answerKey.length(); right++) {
+
+            // New character enters the window
+            if (answerKey.charAt(right) == changeChar) {
+                changeCount++;
+            }
+
+            // More than k changes needed -> shrink
+            while (changeCount > k) {
+
+                if (answerKey.charAt(left) == changeChar) {
+                    changeCount--;
+                }
+
+                left++;
+            }
+
+            // Update longest valid window
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
+        }
+
+        return maxLength;
+    }
+}
