cd06adc37734c30c13ce15399cd41ccfbf9251af
SWETASINGH <sweta_scsbs2024@msit.edu.in>
2026-07-31 09:21:37 +0530

    Time: 1 ms (94.19%), Space: 42.9 MB (44.69%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,28 +1,31 @@
+import java.util.*;
+
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        
 
-        int min = Integer.MAX_VALUE;
         int wCount = 0;
-        for (int i = 0 ; i < k ; i++){
-            if(blocks.charAt(i) == 'W'){
+
+        // Count white blocks in the first window
+        for (int i = 0; i < k; i++) {
+            if (blocks.charAt(i) == 'W') {
                 wCount++;
             }
         }
 
-        min = wCount;
+        int min = wCount;
 
-        for(int i = 0; i< blocks.length() - k; i++) {
+        // Slide the window
+        for (int i = 0; i < blocks.length() - k; i++) {
 
-            if(blocks.charAt(i)== 'W'){
+            if (blocks.charAt(i) == 'W') {
                 wCount--;
             }
 
-            if(blocks.charAt(i+k) == 'W') {
+            if (blocks.charAt(i + k) == 'W') {
                 wCount++;
             }
 
-            min = Math.min(min , wCount);
+            min = Math.min(min, wCount);
         }
 
         return min;
