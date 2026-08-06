8568be0de841806b327c85989e5c64e2eb4a69c3
SWETASINGH <sweta_scsbs2024@msit.edu.in>
2026-08-06 12:11:08 +0530

    Time: 27 ms (43.77%), Space: 111.6 MB (6.84%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer , Integer> map = new HashMap<>();
 
-        HashMap <Integer , Integer > map =new HashMap<>();
-
-        for(int i = 0; i< nums.length; i++) {
-
-            if(map.containsKey(nums[i]))  {
-
-                int previousIndex = map.get(nums[i]);
-
-                if(i - previousIndex <= k) {
+        for(int i =0; i< nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],i);
+                        
+                    
+            }else{
+                int diff = Math. abs (map.get(nums[i]) -i);
+                if(diff<=k){
                     return true;
+
+
                 }
+                else{
+                    map.put(nums[i],i);
+                }
+                
             }
-
-            map.put(nums[i] , i);
         }
-
         return false;
         
     }
